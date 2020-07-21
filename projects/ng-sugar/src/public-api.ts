2794a328b27ee534f0214d898c686f1577f53db8
ambientlight <primary.taras.vozniuk@gmail.com>
2020-07-21 15:03:51 +0800

    bump version, compilation tweaks

--- projects/ng-sugar/src/public-api.ts
+++ projects/ng-sugar/src/public-api.ts
@@ -2,6 +2,15 @@
  * Public API Surface of ng-sugar
  */
 
-export * from './lib/ng-sugar.module';
-export * from './lib/observable-component';
-export * from 'observable-input';
+import { NgSugarModule } from './lib/ng-sugar.module';
+import { ObservableComponent } from './lib/observable-component';
+import { ObservableInput } from 'observable-input';
+import { NgLetDirective } from './lib/ng-let.directive';
+
+export {
+  NgSugarModule,
+  ObservableComponent,
+  ObservableInput,
+  NgLetDirective
+};
+
